f82f1a0769fcdc158c37956f77f2487fe9384bfb
Yirga <rcyg7c@yeah.net>
2023-05-16 15:41:40 +0800

    go template updated and some comment added

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -18,7 +18,7 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
-	mux.Get("/general-quarters", handlers.Repo.Generals)
+	mux.Get("/generals-quarters", handlers.Repo.Generals)
 	mux.Get("/majors-suite", handlers.Repo.Majors)
 	mux.Get("/make-reservation", handlers.Repo.Reservation)
 	mux.Get("/search-availability", handlers.Repo.Availability)
