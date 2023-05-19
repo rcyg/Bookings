f988122cb90bbb18bc4f7030f009aca86fbf4245
Yirga <rcyg7c@yeah.net>
2023-05-19 11:42:30 +0800

    style(forms): adding some comments for test

--- internal/forms/forms_test.go
+++ internal/forms/forms_test.go
@@ -8,21 +8,24 @@ import (
 )
 
 func TestForm_Valid(t *testing.T) {
+	//valid function only checks whether the form received has errors
+	//we can use it for further test function
 	r := httptest.NewRequest("POST", "/whatever", nil)
-	form := New(r.PostForm)
+	form := New(r.PostForm) //got a empty form which has no error
 
-	isValid := form.Valid()
-	if !isValid {
+	isValid := form.Valid() //if empty should be no errors
+	if !isValid {           //if not empty
 		t.Error("got invalid when should have been valid")
 	}
 }
 
 func TestForm_Required(t *testing.T) {
+	//required function adding errors if required field is empty
 	r := httptest.NewRequest("POST", "/whatever", nil)
 	form := New(r.PostForm)
 
-	form.Required("a", "b", "c")
-	if form.Valid() {
+	form.Required("a", "b", "c") //check fields which is empty
+	if form.Valid() {            //it should got errors and return false
 		t.Error("form shows valid when required fields missing")
 	}
 
@@ -35,8 +38,8 @@ func TestForm_Required(t *testing.T) {
 
 	r.PostForm = postedData
 	form = New(r.PostForm)
-	form.Required("a", "b", "c")
-	if !form.Valid() {
+	form.Required("a", "b", "c") //it has these fields thus have no errors actually
+	if !form.Valid() {           //it should return true
 		t.Error("shows does not have required fields when it does")
 	}
 }
@@ -46,7 +49,7 @@ func TestForm_Has(t *testing.T) {
 	form := New(url.Values{})
 
 	has := form.Has("whatever")
-	if has {
+	if has { // it don't has so it should return false
 		t.Error("form shows has field when it does not")
 	}
 
@@ -56,7 +59,7 @@ func TestForm_Has(t *testing.T) {
 	form = New(postedData)
 
 	has = form.Has("a")
-	if !has {
+	if !has { //it actually has so it should return true
 		t.Error("shows form does not have field when it should")
 	}
 }
@@ -66,12 +69,12 @@ func TestForm_MinLength(t *testing.T) {
 	form := New(postedData)
 
 	form.MinLength("x", 10)
-	if form.Valid() {
+	if form.Valid() { //it has errors so it should be false
 		t.Error("form shows min length for non-existent field")
 	}
 
 	isError := form.Errors.Get("x")
-	if isError == "" {
+	if isError == "" { //it actually got errors so the isError should not be blank
 		t.Error("should have error but did not get one")
 	}
 
@@ -81,7 +84,7 @@ func TestForm_MinLength(t *testing.T) {
 	form = New(postedData)
 
 	form.MinLength("some_field", 100)
-	if form.Valid() {
+	if form.Valid() { //not satify the length requirement it got errors
 		t.Error("shows min length of 100 met when data is shorter")
 	}
 
@@ -90,12 +93,12 @@ func TestForm_MinLength(t *testing.T) {
 	form = New(postedData)
 
 	form.MinLength("another_field", 1)
-	if !form.Valid() {
+	if !form.Valid() { //satisfy the length requirement so it will pass
 		t.Error("shows min length if 1 is not met when it is")
 	}
 
 	isError = form.Errors.Get("another_field")
-	if isError != "" {
+	if isError != "" { //it pass the length requirement so it will pass
 		t.Error("should not have error but got one")
 	}
 
@@ -106,7 +109,7 @@ func TestForm_IsEmail(t *testing.T) {
 	form := New(postedValues)
 
 	form.IsEmail("x")
-	if form.Valid() {
+	if form.Valid() { //field not exist, shall not pass
 		t.Error("form shows valid email for non-existent field")
 	}
 
@@ -115,7 +118,7 @@ func TestForm_IsEmail(t *testing.T) {
 	form = New(postedValues)
 
 	form.IsEmail("email")
-	if !form.Valid() {
+	if !form.Valid() { //actually a email, should pass
 		t.Error("got an invalid email when we should not have")
 	}
 
@@ -124,7 +127,7 @@ func TestForm_IsEmail(t *testing.T) {
 	form = New(postedValues)
 
 	form.IsEmail("email")
-	if form.Valid() {
+	if form.Valid() { //invalid email, shall not pass
 		t.Error("got valid for invalid email address")
 	}
 }
