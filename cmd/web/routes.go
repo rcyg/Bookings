ec021eb72991a4d60b667638203c45ed948b3957
Yirga <rcyg7c@yeah.net>
2023-05-28 19:17:00 +0800

    feat : optimization of dashboard

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -47,6 +47,9 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Route("/admin", func(mux chi.Router) {
 		mux.Use(Auth)
 		mux.Get("/dashboard", handlers.Repo.AdminDashboard)
+		mux.Get("/reservation-new", handlers.Repo.AdminNewReservations)
+		mux.Get("/reservation-all", handlers.Repo.AdminAllReservations)
+		mux.Get("/reservations-calendar", handlers.Repo.AdminReservationsCalendar)
 	})
 
 	return mux
